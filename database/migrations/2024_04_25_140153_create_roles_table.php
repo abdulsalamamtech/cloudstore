e5a660dd028d6494be84f26d1eeb78300eba2128
AbdulsalamAmtech <abdulsalamamtech@gmail.com>
2024-04-26 17:43:08 +0100

    Modified migration file

--- database/migrations/2024_04_25_140153_create_roles_table.php
+++ database/migrations/2024_04_25_140153_create_roles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users','id')->cascadeOnDelete();
-            $table->string('name');
+            $table->enum('name', ['user', 'admin']);
             $table->boolean('status')->default(false);
             $table->softDeletes();
             $table->timestamps();
