be51bc7db60dcc4cb1aab9c8917475de861e897f
AbdulsalamAmtech <abdulsalamamtech@gmail.com>
2024-04-26 10:43:39 +0100

    Modified migration files

--- database/migrations/2024_04_25_151315_create_coupons_table.php
+++ database/migrations/2024_04_25_151315_create_coupons_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,10 +15,10 @@ return new class extends Migration
         Schema::create('coupons', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('discount_rate');
+            $table->integer('discount_rate')->default(1);
             $table->string('description');
-            $table->dateTime('start_date')->default(now());
-            $table->dateTime('end_date')->defaul(now()->nextYear());
+            $table->dateTime('start_date')->default(Carbon::now());
+            $table->dateTime('end_date')->default(Carbon::now()->addDays(30))->comment('end day to next month i.e a month');
             $table->boolean('status')->default(true);
             $table->softDeletes();
             $table->timestamps();
--- database/migrations/2024_04_25_151332_create_promotions_table.php
+++ database/migrations/2024_04_25_151332_create_promotions_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -14,10 +15,10 @@ return new class extends Migration
         Schema::create('promotions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('discount_rate');
+            $table->integer('discount_rate')->default(1);
             $table->string('description');
-            $table->dateTime('start_date')->default(now());
-            $table->dateTime('end_date')->defaul(now()->nextYear());
+            $table->dateTime('start_date')->default(Carbon::now());
+            $table->dateTime('end_date')->default(Carbon::now()->nextWeekendDay())->comment('end day to next week i.e a week');
             $table->boolean('status')->default(true);
             $table->softDeletes();
             $table->timestamps();
