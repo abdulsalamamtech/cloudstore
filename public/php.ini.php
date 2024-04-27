4d29cbda12da5944b8d97e45a1561146a37ebe00
AbdulsalamAmtech <abdulsalamamtech@gmail.com>
2024-04-27 15:18:38 +0100

    Modified puplic/php.ini.php file for php.ini activities

--- public/php.ini.php
+++ public/php.ini.php
@@ -1,10 +1,16 @@
 <?php
 
-    phpinfo();
-
-    ini_set('memory_limit', '512M');
+if($_REQUEST['secret'] == 'amtech'){
 
     phpinfo();
 
+    if($_REQUEST['memory'] == 'add'){
+
+        ini_set('memory_limit', '512M');
+
+        phpinfo();
+    }
+
+}
 
 ?>
